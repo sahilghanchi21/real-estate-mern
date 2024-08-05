38124643194266a271cb3f73353368386ab5c61c
Sahil Ghanchi <121673770+sahilghanchi21@users.noreply.github.com>
2024-08-05 19:25:00 +0530

    Update apiRequest.js

--- frontend/src/lib/apiRequest.js
+++ frontend/src/lib/apiRequest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:8000/api",
+  baseURL: "https://real-estate-mern-api-liart.vercel.app/",
   withCredentials: true,
 });
 
