3d393e2bd5b70a4d8a76961c348efb057b832505
Sahil Ghanchi <ghanchisahil819@gmail.com>
2024-08-05 18:54:06 +0530

    all done

---
+++ frontend/src/components/list/List.jsx
@@ -0,0 +1,15 @@
+import './list.scss'
+import Card from "../card/Card"
+
+function List({ posts }) {
+  console.log(posts)
+  return (
+    <div className='list'>
+      {posts.map(item => (
+        <Card key={item.id} item={item} />
+      ))}
+    </div>
+  )
+}
+
+export default List
---
+++ frontend/src/components/navbar/Navbar.jsx
@@ -0,0 +1,81 @@
+import { useContext, useState } from "react";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
+import { AuthContext } from "../../context/AuthContext";
+import { useNotificationStore } from "../../lib/notificationStore";
+
+function Navbar() {
+  const [open, setOpen] = useState(false);
+
+  const { currentUser } = useContext(AuthContext);
+
+  const fetch = useNotificationStore((state) => state.fetch);
+  const number = useNotificationStore((state) => state.number);
+
+  if (currentUser) fetch();
+
+  return (
+    <nav>
+      <div className="left">
+        <a href="/" className="logo">
+          <img src="/logo.png" alt="" />
+          <span>LamaEstate</span>
+        </a>
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        {currentUser ? (
+          <div className="user">
+            <img src={currentUser.avatar || "/noavatar.jpg"} alt="" />
+            <span>{currentUser.username}</span>
+            <Link to="/profile" className="profile">
+              {number > 0 && <div className="notification">{number}</div>}
+              <span>Profile</span>
+            </Link>
+          </div>
+        ) : (
+          <>
+            <a href="/login">Sign in</a>
+            <a href="/register" className="register">
+              Sign up
+            </a>
+          </>
+        )}
+        <div className="menuIcon">
+          <img
+            src="/menu.png"
+            alt=""
+            onClick={() => setOpen((prev) => !prev)}
+          />
+        </div>
+        <div className={open ? "menu active" : "menu"}>
+          <Link to="/">Home</Link>
+          <Link to="/">About</Link>
+          <Link to="/">Contact</Link>
+          <Link to="/">Agents</Link>
+          {currentUser ? (
+            <div className="user">
+              <span>{currentUser.username}</span>
+              <Link to="/profile" className="profile">
+                <div className="notification">3</div>
+                <span>Profile</span>
+              </Link>
+            </div>
+          ) : (
+            <>
+              <Link to="/login">Sign in</Link>
+              <Link to="/register" className="register">
+                Sign up
+              </Link>
+            </>
+          )}
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
